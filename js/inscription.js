7933cb54c041d7cc2980b362735ed9580e0cea8c
cybertechangel <nelnihadnel@gmail.com>
2025-05-15 13:15:23 +0200

    modif js

--- js/inscription.js
+++ js/inscription.js
@@ -2,4 +2,4 @@ document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("registration-form");
     const select = document.getElementById("tournament-select");
     fetch("../data/tournaments.json")
-    
+})
