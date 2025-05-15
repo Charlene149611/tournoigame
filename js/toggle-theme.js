8c75449086755401064db90df973943e3261d53b
cybertechangel <nelnihadnel@gmail.com>
2025-05-15 10:33:13 +0200

    init

---
+++ js/toggle-theme.js
@@ -0,0 +1,26 @@
+// Fonction pour récupérer le theme depuis le localStorage
+
+const currentTheme = localStorage.getItem('theme') || 'light';
+
+// Appliquer le thème stocké
+
+if (currentTheme === 'dark') {
+    document.body.classList.add('dark-mode');
+    document.getElementById('theme-toggle').innerHTML = '<i class="fas fa-sun"></i>';
+} else {
+    document.body.classList.remove('dark-mode');
+    document.getElementById('theme-toggle').innerHTML = '<i class="fas fa-moon"></i>';
+}
+
+// On va ajouter l'évenement pour changer le thème
+
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    const isDarkMode = document.body.classList.toggle('dark-mode');
+    
+    // On va sauvegarder le thème dans le localStorage
+    localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+
+    // On va changer l'icône du bouton
+    this.innerHTML = isDarkMode ? '<i class="fas fa-moon"></i>' : '<i class="fas fa-sun"></i>';
+}
+);
